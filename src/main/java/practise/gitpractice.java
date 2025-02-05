71ded9bb958409c135c8497756915c3befb6dbd3
91975 <91975@DHARUN_10>
2025-02-05 13:14:33 +0530

    commit

--- src/main/java/practise/gitpractice.java
+++ src/main/java/practise/gitpractice.java
@@ -7,7 +7,7 @@ public class gitpractice {
 	public void demo()
 	{
 		System.out.println("it is my first push operation");
-		System.out.println("it is my first push    operation");
+		System.out.println("it is my first push operation");
 	}
 
 }
