5b0e69c8e8b975f1ddff578c8dee5157396c6b54
91975 <91975@DHARUN_10>
2025-02-05 11:53:13 +0530

    this is my second pro

---
+++ src/main/java/practise/gitpractice.java
@@ -0,0 +1,12 @@
+package practise;
+
+import org.testng.annotations.Test;
+
+public class gitpractice {
+	@Test
+	public void demo()
+	{
+		System.out.println("it is my second push operation");
+	}
+
+}
